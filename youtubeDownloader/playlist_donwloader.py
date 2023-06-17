99b66d37a3ae3ecb2767deaa98a5ae9cdea5228b
antonislouca <alouka04@cs.ucy.ac.cy>
2023-06-17 16:23:05 +0300

    Update playlist_donwloader.py

--- youtubeDownloader/playlist_donwloader.py
+++ youtubeDownloader/playlist_donwloader.py
@@ -5,25 +5,38 @@ from pathlib import Path
 import os
 # print(Path.home())
 # url = input("Enter Playlist URL: ")
+
 result_dir = f'{str(Path.home())}/Desktop/videos/'
 
 if not os.path.isdir(result_dir):
     os.mkdir(result_dir)
+
 playlists = ['https://www.youtube.com/watch?v=2Y7eyyFi6KQ&list=PL_EFQ32UJ6ZuoNOREZTajBBU_zErhzAi9',
-             'https://www.youtube.com/watch?v=g4Z353jBwsc&list=PLMJ78NDkO-sMS7aSBg-X_NoVIDQmDVyIb', 'https://www.youtube.com/watch?v=BrPv9hRhlQg&list=PLEJ8Ygunw55eSvFfppUBAv87DJS6tPdA1', '']
+             'https://www.youtube.com/watch?v=g4Z353jBwsc&list=PLMJ78NDkO-sMS7aSBg-X_NoVIDQmDVyIb',
+             'https://www.youtube.com/watch?v=BrPv9hRhlQg&list=PLEJ8Ygunw55eSvFfppUBAv87DJS6tPdA1',
+             'https://www.youtube.com/watch?v=Fr2_fJmN1Vw&list=PLsScJ6QVjnirqR6H-IaJLnn6YvTSVM4jT&pp=iAQB']
+
 directories = ['Thapse', 'Cooking', 'Broscar', 'Standup']
 
 for i, pl in enumerate(playlists):
     playlist = Playlist(pl)
+
     print("Total Videos: ", len(playlist.video_urls),
           'for playlist: ', directories[i])
+
     if not os.path.isdir(f'{result_dir}/{directories[i]}'):
         os.mkdir(f'{result_dir}/{directories[i]}')
 
-    for video_url in playlist.video_urls:
-        yt = YouTube(video_url, on_progress_callback=on_progress)
-        stream = yt.streams.get_highest_resolution()
-        print(yt.title)
-        filedest = f'{result_dir}/{directories[i]}/{yt.title}.mp4'
-        stream.download(
-            filename=filedest)
+    for index, video_url in enumerate(playlist.video_urls):
+        try:
+            yt = YouTube(video_url)
+
+            stream = yt.streams.get_highest_resolution()
+            print(yt.title)
+
+            filedest = f'{result_dir}/{directories[i]}/{yt.title}.mp4'
+
+            stream.download(filename=filedest)
+        except:
+            print(f"Skipping age-restricted video: {video_url}")
+            continue
